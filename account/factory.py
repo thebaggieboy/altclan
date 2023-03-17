8cc1d6a75bdbbe75398812dcc2163f2f154a88b4
Enimofe Odujirin <daemonargo@protonmail.com>
2023-03-17 04:46:48 +0100

    Profile

--- account/factory.py
+++ account/factory.py
@@ -1,9 +1,25 @@
 import factory 
-from django.contrib.auth.models import User
+from django.conf import settings
 from factory.faker import faker
-from .models import Profile
+from .models import Profile, CustomUser
+
+User = settings.AUTH_USER_MODEL
+FAKE = faker.Faker()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    email = factory.Faker('email')
 
 class ProfileFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Profile
-        
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    #email_address = factory.Faker('email')
+    mobile_number = factory.Faker('phone_number')
+    #user = factory.SubFactory(UserFactory)
+
+
+
+
